0753a1a9f051039197532029a60272ecfd91d9c8
Adjoa Hackman <adjoahackman93@gmail.com>
2024-03-11 18:07:34 -0400

    pushing my first commit. Had trouble with cloning my repo

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Adjoa-s-Calendar
+
+
+1. Get parent of button clicked (already done)
+2. For that parent, get id. - https://stackoverflow.com/questions/3239598/how-can-i-get-the-id-of-an-element-using-jquery 
+3. From that id get hour number. id is in format "hour-X" and you need to get "X". You can look at something like https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
+4. Set event name in local storage for that hour number
---
+++ script.js
@@ -0,0 +1,90 @@
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// the code isn't run until the browser has finished rendering all the elements
+// in the html.
+$(function () {
+  // var advancedFormat = require('dayjs/plugin/advancedFormat')
+  // dayjs.extend(advancedFormat) 
+  const currentDate = dayjs();
+  const formattedDate = currentDate.format('dddd, MMMM D');
+  $("#currentDay").text(formattedDate)
+  console.log(formattedDate);
+  const currentTime = parseInt(currentDate.format('H'));
+  // $()
+  // alert(currentTime)
+  // if (currentTime > 9) {
+  //   $("#hour-9").addClass("future");
+
+  // } 
+  for (var i=9; i<18; i++) {
+    var idDiv = "#hour-"+i
+    if (i == currentTime) {
+      $(idDiv).addClass("present");
+      // alert("present")
+    }else if (i < currentTime) {
+      $(idDiv).addClass("past");
+      // alert("past")
+    }else {
+      $(idDiv).addClass("future")
+      // alert("future")
+    } 
+  }
+  $(".saveBtn").click(function(){
+    $(this).parent();
+    var container = $(this).parent();
+    var containerId = container.attr("id");
+    // alert("id");
+    
+    var savedText = document.querySelector(".text-center").value;
+    container.textContent = savedText; 
+    localStorage.setItem("text",savedText);
+
+    // console.log()
+    // container.find(".text-center");
+
+    
+
+    // var savedText = JSON.parse(localStorage.getItem("description"));
+    // if (savedText) {
+    //   descriptionSavedText = savedText;
+    // }
+    // localStorage.setItem("description",JSON.stringify(savedText))
+
+    // var savedText =localStorage.setItem("description")
+    // if (savedText) {
+    //   savedText.push = document.querySelector("text-center")
+    // } else {
+    //   savedText = []
+    // }
+  })
+})
+    // var saveText = document.querySelector("description").value;
+    // if (localStorage.getItem("description")) {
+    //   field.value = localStorage.getItem("autosave");
+    // }
+    // field.addEventListener("change",() => {
+    //   localStorage.setItem("autosave",field.value);
+    // })
+//     }
+// })
+//"this" is in reference to the element that the function is attached to - which in this, is the save button. 
+// function? How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked?
+  //get the parent of the button and we will have the element that has the ID
+//How might the id be useful when saving the description in local storage?
+  //when I click the button, figure out what hour its for, then once we have the hour, store the event name that I defined for the hour}
+
+  //depending on the hour, we need the class of present, future or past
+//only need the hour for the daily planner
+  // const f=currentDate.format('dddd, MMMM Do')
+  // alert(formattedDate);
+  // alert(f)
+  // TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage. HINT: What does `this` reference in the click listener function? How can DOM traversal be used to get the "hour-x" id of the// time-block containing the button that was clicked? How might the id be // useful when saving the description in local storage?
+  //
+  // TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour. HINTS: How can the id attribute of each time-block be used to conditionally add or remove the past, present, and future classes? How can Day.js be used to get the current hour in 24-hour time?
+  //
+  // TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements. HINT: How can the id attribute of each time-block be used to do this?
+  //
+  // TODO: Add code to display the current date in the header of the page.
+
+  //need to put a period before the class name here like css
+  // var nineam = document.querySelector()
+  //trying to use this source for the local storage https://developer.mozilla.org/en-US/docs/Web/API/Window/sessionStorage 
